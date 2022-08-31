6e84eaa53beae45e7044c23f841c3de4d69dcce7
Valentina <vkpavlenina@mail.ru>
2022-08-31 09:00:25 +0500

    Task_10 project

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трёхзначное число ");
+        string number = Console.ReadLine();
+        Console.WriteLine(number[1]);
+    }
+}
