81cdaa42011bb4a8bb5abfd7e7f2ed8ea65bf7a5
Valentina <vkpavlenina@mail.ru>
2022-08-31 17:38:42 +0500

    Task_13 project

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 99)
+        {
+            Console.WriteLine(number.ToString()[2]);
+        }
+        else Console.WriteLine("Третьей цифры нет");
+    }
+}
