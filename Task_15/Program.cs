c88f05efd27e10aeef945cb4a0db18cd222e05d5
Valentina <vkpavlenina@mail.ru>
2022-08-31 18:16:42 +0500

    Task_15 project

---
+++ Task_15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void WriteSelect(string[] namesArray)
+    {
+        for (int i = 0; i < namesArray.Length - 1; i++)
+        {
+            Console.Write(i + 1 + " - " + namesArray[i] + ", ");
+        }
+        Console.WriteLine(namesArray.Length + " - " + namesArray[namesArray.Length - 1] + "."); // Чтобы вывести последний элемен с точкой    }
+        // Console.WriteLine(""); исскуственно с новой строки
+    }
+    static void SelectDayWeek()
+    {
+        string[] namesDayWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        WriteSelect(namesDayWeek);
+        Console.WriteLine("Укажите день недели ");
+        int indexDayWeek = Convert.ToInt32(Console.ReadLine());
+        if (indexDayWeek < 6)
+            Console.WriteLine("Является выходным? Ответ: нет  ");
+        if (indexDayWeek > 5)
+            Console.WriteLine("Является выходным? Ответ: да  ");
+    }
+    static void Main(string[] args)
+    {
+        SelectDayWeek();
+    }
+}
